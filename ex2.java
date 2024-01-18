6ec90e21bbc8849c5a36f88bf814956cfa26f10d
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:51:26 +0200

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,15 @@
+public class Tester2 {
+
+    public void f2(int number)
+    {
+        System.out.println(number / 5);
+    }
+
+
+    public static void main(String[]args)
+    {
+        Tester2 test = new Tester2();
+        test.f2(15);
+
+    }
+}
