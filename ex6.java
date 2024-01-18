e469cad9b32ea06134dd714704b8abe2abc48702
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:54:19 +0200

    Create ex6.java

---
+++ ex6.java
@@ -0,0 +1,20 @@
+public class Tester6 {
+    public double f6 ( int a, int b){
+        double c  = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return c;
+
+
+
+
+    }
+
+
+
+    public static void main(String[]args){
+
+        Tester6 test = new Tester6();
+        System.out.println(test.f6(3,3));
+
+
+    }
+}
