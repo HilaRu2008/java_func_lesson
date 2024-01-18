bf1d97999d095997183343cc2cc02929605ace82
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:51:55 +0200

    Create ex3.java

---
+++ ex3.java
@@ -0,0 +1,17 @@
+public class Tester3
+{
+    public int f3(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[]args) {
+        Tester3 test = new Tester3();
+        int number = test.f3(2, 3);
+        System.out.println(number);
+    }
+
+
+
+
+
+}
