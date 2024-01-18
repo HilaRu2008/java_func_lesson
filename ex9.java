2fa325dcf6fb13ff078ef710eb9f5c495b45c635
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:55:47 +0200

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,14 @@
+public class Tester9 {
+
+    public static double f9 (String text){
+        double num = Math.pow(text.length(), 2);
+        return num;
+
+    }
+
+    public static void main(String[]args){
+
+        System.out.println(f9("hello"));
+
+    }
+}
