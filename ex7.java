458ebde9964c266a9db934973784fb0569366918
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:54:48 +0200

    Create ex7.java

---
+++ ex7.java
@@ -0,0 +1,19 @@
+public class Tester7 {
+    public void f7 (char letter1, char letter2, char letter3){
+        System.out.println("" + letter3 + letter2 + letter1);
+
+
+
+
+    }
+
+
+
+    public static void main(String[]args){
+
+        Tester7 test = new Tester7();
+        test.f7('a', 'b', 'c');
+
+
+    }
+}
