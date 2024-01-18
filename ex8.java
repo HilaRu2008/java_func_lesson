1520a4ba823420bbb9877114375b1a3553f929d7
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-01-18 10:55:20 +0200

    Create ex8.java

---
+++ ex8.java
@@ -0,0 +1,16 @@
+public class Tester8 {
+
+    public static int f8(int a1, int d, int n){
+        int an = a1 + d * (n - 1);
+        return an;
+    }
+
+
+
+    public static void main(String[]args){
+
+        System.out.println(f8(2, 2,5));
+
+
+    }
+}
