f5de4e0d354b5df8d3f0be9beb985c20225e0042
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2024-02-07 21:12:00 +0200

    Update loops_lesson.java

--- loops_lesson.java
+++ loops_lesson.java
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 
 import java.util.Scanner;
 
@@ -220,20 +228,20 @@ public class Main {
         int a = 1;
         int b = 1;
         
-        for (int i=0; i < index2; i++) {  // i = 3
+        for (int i=1; i < index2; i++) {  // i = 3
             
             
             if (i % 2 == 0){
                 if (i >= index1)
-                    System.out.println(a);
-                a += b;
+                    System.out.println(b);
+                b += a;
             }
                 
             else{
                 
                 if (i >= index1)
-                    System.out.println(b);
-                b += a;
+                    System.out.println(a);
+                a += b;
                 
             }
                 
@@ -242,14 +250,56 @@ public class Main {
     
     
     
-    /// public
+    public static void f13(int h){
+        
+        int num_of_asterisks_in_the_sub_base = h + (2 * (h - 1));
+        //System.out.println(num_of_asterisks_in_the_sub_base);  // working
+        
+        
+        for(int col = 0; col < h; col++){
+            
+            int num_of_asterisks_in_every_line = h + (2 * col);
+            int needed_spaces_on_each_side = (num_of_asterisks_in_the_sub_base - num_of_asterisks_in_every_line) / 2;
+            
+            //System.out.println("num of asterisks in every line: " + num_of_asterisks_in_every_line); // working
+            //System.out.println("needed spaces on each side: " + needed_spaces_on_each_side);  // working
+            
+            
+            for (int row = 0; row < num_of_asterisks_in_the_sub_base; row++){
+                
+                if (row < num_of_asterisks_in_the_sub_base - num_of_asterisks_in_every_line - needed_spaces_on_each_side  ||  row >= num_of_asterisks_in_the_sub_base - needed_spaces_on_each_side){ 
+                    
+                    //row < num_of_asterisks_in_the_sub_base - num_of_asterisks_in_every_line - needed_spaces_on_each_side:
+                    // check if the index of the row is smaller than the index of the first astrisk --> print a space if so
+                    
+                    //row >= num_of_asterisks_in_the_sub_base - needed_spaces_on_each_side: 
+                    // checks if the row (index) got to a index we need to start printing spaces again (the condition gets the index after the last index asterisk in a row  
+                    
+                    System.out.print(" ");
+                }else {
+                    System.out.print("*");
+                }
+                
+            }
+            System.out.println();
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
 
 
 
     public static void main(String[]args){
 
         // System.out.println(f8(6));
-        f12(2,5);
+        f13(5);
 
     }
 
